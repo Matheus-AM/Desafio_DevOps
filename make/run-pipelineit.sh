2472ff39021ea7a20f4d591e9008f6dbf2fbac3f
Matheus-AM <mam.macacod@gmail.com>
2025-08-27 12:34:14 -0300

    Entrega final enxuta v0.0.1

--- make/run-pipelineit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export KUBE_PATH='/mnt/c/Users/****/.kube' # path do seu .kube
-docker build . -f "local-pipelines/Dockerfile.itrunner" -t "app-runner"
-docker run -it --rm -v $KUBE_PATH:/root/.kube:ro -v /var/run/docker.sock:/var/run/docker.sock --name "app-runner" --network=registry-net app-runner-it
